9860709d560786360e106f10211d54e9d6668620
AngelCheng <c6150261602@yahoo.com.tw>
2019-10-29 00:11:20 +0800

    add assets and clip script

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] models;
+    enum GameState
+    {
+        SELECT_MODEL, DECIDE_SCALE, DECIDE_DIRCTION, DECIDE_PSITION, PLACE_MODEL, FINISH
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerControl();
+    }
+    void playerControl()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+    }
+}
